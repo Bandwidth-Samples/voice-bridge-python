4e2bd760faa7d66d18534e5ff2693758c52294a4
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2024-01-10 10:47:06 -0500

    SWI-3510 Update Sample App to target new python SDK

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from bandwidth.bandwidth_client import BandwidthClient
-from bandwidth.voice.models.api_create_call_request import ApiCreateCallRequest
-from bandwidth.voice.bxml.response import Response
-from bandwidth.voice.bxml.verbs import *
-
-from flask import Flask, request
-
+import http
 import os
 import sys
-import json
+
+import bandwidth
+from bandwidth.models.bxml import Response as BxmlResponse
+from bandwidth.models.bxml import SpeakSentence, Ring, Bridge
+from fastapi import FastAPI, Response
+import uvicorn
 
 try:
     BW_USERNAME = os.environ['BW_USERNAME']
@@ -16,61 +15,56 @@ try:
     BW_VOICE_APPLICATION_ID = os.environ['BW_VOICE_APPLICATION_ID']
     BW_NUMBER = os.environ['BW_NUMBER']
     USER_NUMBER = os.environ['USER_NUMBER']
-    LOCAL_PORT = os.environ['LOCAL_PORT']
+    LOCAL_PORT = int(os.environ['LOCAL_PORT'])
     BASE_CALLBACK_URL = os.environ['BASE_CALLBACK_URL']
-except:
-    print("Please set the environmental variables defined in the README")
+except KeyError as e:
+    print(f"Please set the environmental variables defined in the README\n\n{e}")
+    sys.exit(1)
+except ValueError as e:
+    print(f"Please set the LOCAL_PORT environmental variable to an integer\n\n{e}")
     sys.exit(1)
 
-bandwidth_client = BandwidthClient(
-    voice_basic_auth_user_name=BW_USERNAME,
-    voice_basic_auth_password=BW_PASSWORD
+app = FastAPI()
+
+bandwidth_configuration = bandwidth.Configuration(
+    username=BW_USERNAME,
+    password=BW_PASSWORD
 )
 
-voice_client = bandwidth_client.voice_client.client
+bandwidth_api_client = bandwidth.ApiClient(bandwidth_configuration)
+bandwidth_calls_api_instance = bandwidth.CallsApi(bandwidth_api_client)
 
-app = Flask(__name__)
 
-@app.route('/callbacks/inboundCall', methods=['POST'])
-def inbound_call():
-    callback_data = json.loads(request.data)
-
-    body = ApiCreateCallRequest()
-    body.mfrom = BW_NUMBER
-    body.to = USER_NUMBER 
-    body.answer_url = BASE_CALLBACK_URL + '/callbacks/outboundCall' 
-    body.application_id = BW_VOICE_APPLICATION_ID
-    body.tag = callback_data['callId']
-
-    voice_client.create_call(BW_ACCOUNT_ID, body=body)
-
-    response = Response()
-    speak_sentence = SpeakSentence(
-        sentence="Hold while we connect you."
-    )
-    ring = Ring(
-        duration=30
+@app.post('/callbacks/inboundCall', status_code=http.HTTPStatus.OK)
+def inbound_call(inbound_callback: bandwidth.models.AnswerCallback):
+    call_body = bandwidth.models.CreateCall(
+        to=USER_NUMBER,
+        from_=BW_NUMBER,
+        answer_url=f"{BASE_CALLBACK_URL}/callbacks/outboundCall",
+        application_id=BW_VOICE_APPLICATION_ID,
+        tag=inbound_callback.call_id
     )
 
-    response.add_verb(speak_sentence)
-    response.add_verb(ring)
-    return response.to_bxml()
+    try:
+        bandwidth_calls_api_instance.create_call(BW_ACCOUNT_ID, call_body=call_body)
+    except bandwidth.ApiException as e:
+        print(f"Error creating call: {e}")
 
-@app.route('/callbacks/outboundCall', methods=['POST'])
-def outbound_call():
-    callback_data = json.loads(request.data)
+    speak_sentence = SpeakSentence(text="Hold while we connect you.")
+    ring = Ring(duration=30)
+    bxml_response = BxmlResponse([speak_sentence, ring])
 
-    response = Response()
-    speak_sentence = SpeakSentence(
-        sentence="Hold while we connect you. We will begin to bridge you now."
-    )
-    bridge = Bridge(
-        call_id=callback_data['tag']
-    )
+    return Response(content=bxml_response.to_bxml(), media_type="application/xml")
 
-    response.add_verb(speak_sentence)
-    response.add_verb(bridge)
-    return response.to_bxml()
+
+@app.post('/callbacks/outboundCall', status_code=http.HTTPStatus.OK)
+def outbound_call(inbound_callback: bandwidth.models.AnswerCallback):
+    speak_sentence = SpeakSentence(text="Hold while we connect you. We will begin to bridge you now.")
+    bridge = Bridge(target_call=inbound_callback.tag)
+    bxml_response = BxmlResponse([speak_sentence, bridge])
+
+    return Response(content=bxml_response.to_bxml(), media_type="application/xml")
+
 
 if __name__ == '__main__':
-    app.run(port=LOCAL_PORT)
+    uvicorn.run("main:app", port=LOCAL_PORT, reload=True)
